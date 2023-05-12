5b46df0fbc1b4b62eda2ca9dc00653199c6f996f
Eric Julianto <algonacci1@gmail.com>
2023-05-12 17:25:00 +0700

    Setup constant variables and google fonts

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Constants {
+  static const mainColor = Colors.deepPurple;
+  static const String title = 'Calculate Average';
+  static final TextStyle textStyle = GoogleFonts.lato(
+    fontSize: 20,
+    color: mainColor,
+  );
+}
